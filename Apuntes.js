8ed66195a2453e009e14038329196ce63c6a1c96
frank yanez <frankyanez9413@gmail.com>
2023-04-21 16:56:29 -0300

    clase form

---
+++ Apuntes.js
@@ -0,0 +1,17 @@
+/*
+Un #id al punto donde deseo refernciar y luego hacer un <a href="#id"></a> que me redireccione al id que tengo arriba
+
+
+./ = misma carpeta
+../ una carpeta para atras
+
+display: inline = solo ocupa lo que mide
+display: inline block = ocupa todo el ancho
+
+naranja = margin
+verde = padding
+
+para alinear con justify + align se necesita saber cuanto mide el padre
+
+left: 0 
+*/
